b93633da2a01ead4fcba6500a2e15f687f4ebd99
neo <chuymedina96@gmail.com>
2019-04-10 18:32:36 -0500

    more

--- JS/Async_Foundations/forEach_functions/main.js
+++ JS/Async_Foundations/forEach_functions/main.js
@@ -33,3 +33,9 @@ forEach(strings, function(str, index, array){
     result += str + "!!!";
   }
 });
+
+function forEach(arr, callback){
+  for(var i = 0; i < arr.length; i++){
+    callback(arr[i], i , arr);
+  }
+}
