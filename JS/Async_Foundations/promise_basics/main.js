67eff783a50595a45cdeb3784368a665f1fc2c55
neo <chuymedina96@gmail.com>
2019-04-12 01:54:23 -0500

    more

--- JS/Async_Foundations/promise_basics/main.js
+++ JS/Async_Foundations/promise_basics/main.js
@@ -28,3 +28,29 @@ var p1 = new Promise(function(resolve, reject){
 p1.then(function(arr){
   console.log("Promise p1 resolved with data: ", arr);
 });
+
+var p1= new Promise(function(resolve, reject){
+  reject("Error");
+});
+
+p1.then(function(data){
+  console.log("Promise p1 resolved with data:", data);
+}).catch(function(data){
+  console.log("Promise p1 was rejected with data:", data);
+});
+
+//Promise - with Randomly Ocurring error
+
+var p1 = new Promise(function(resolve, reject){
+  var num = Math.random();
+  if(num < 0.5){
+    resolve(num);
+  }else{
+    reject(num);
+  }
+
+p1.then(function(result){
+  console.log("Success:", result);
+}).catch(function(error){
+  console.log("Error:", error);
+});
