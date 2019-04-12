503863d651d2aa02c63d05e68bd14cfe93d20857
neo <chuymedina96@gmail.com>
2019-04-12 02:07:11 -0500

    more on promises and promise chaining

--- JS/Async_Foundations/promise_basics/main.js
+++ JS/Async_Foundations/promise_basics/main.js
@@ -55,3 +55,14 @@ p1.then(function(result){
 }).catch(function(error){
   console.log("Error:", error);
 });
+
+var promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    var randomInt = Math.floor(Math.random() * 10);
+    resolve(randomInt);
+  }, 4000);
+});
+
+promise.then(function(data){
+  console.log("Random int passed to resolve:", data);
+});
---
+++ JS/Async_Foundations/promise_chaining/main.js
@@ -0,0 +1,8 @@
+/*
+  Objectives
+
+    - Describe the disadvantages of using nested callbacks
+    - Return a promise from a .then callback function
+    - use a promise to make asynchronous code seem sequential
+
+*/
