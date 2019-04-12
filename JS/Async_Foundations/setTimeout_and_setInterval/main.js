bd10cac6986ce4fb33aa943ab910d6d42b163378
neo <chuymedina96@gmail.com>
2019-04-11 23:18:07 -0500

    more on setTimeout and set_Interval

--- JS/Async_Foundations/setTimeout_and_setInterval/main.js
+++ JS/Async_Foundations/setTimeout_and_setInterval/main.js
@@ -5,9 +5,14 @@
 
     setTimeout
       - A function that asynchronously invokes a callback after a delay in milliseconds
+
+    setInterval
+      - A function that continually invokes a callback after every X milliseconds,
+        where X provided to setInterval.
 */
 
 // setTimeout usage
+/*
 function callback(){
   console.log("callback function");
 }
@@ -28,3 +33,67 @@ setTimeout(function(){
   console.log("I am canceling the first setTimeout", timerId);
   clearTimeout(timerId);
 }, 2000)
+
+
+function callback(){
+  console.log("callback is called continuously");
+}
+var repeat = 3000;
+
+setInterval(callback, repeat); //repeat it the interval in ms.
+
+//setInterval Example
+
+var num = 0;
+var int = 1000;
+setInterval(function(){
+  num++;
+  console.log("num:", num);
+}, int);
+
+
+//Cancelling setInterval
+var num = 0;
+var intervalId = setInterval(function(){
+  num++;
+  console.log("num:", num);
+  if(num === 3){
+    clearInterval(intervalId);
+  }
+}, 1000);
+*/
+
+
+/*Your goal is to implement a function called countDown.
+The function will accept 1 parameter which is a time in seconds
+for the count down.  The function should console.log the time
+remaining every second.
+Once the timer gets to 0, the timer should be stopped and you should
+console.log "Ring Ring Ring!!!".
+
+HINT: You will need to use setInterval() to count down and clearInterval to stop the timer.
+
+Example:
+
+countDown(3)
+
+Console output
+
+    Timer: 2
+    Timer: 1
+    Ring Ring Ring!!!
+    */
+
+
+var num = 11;
+
+var myInterval = 1000;
+
+var countDown = setInterval(function(seconds){
+  num--;
+  console.log("num:", num);
+  if(num === 0){
+    console.log("Ring ring ring!!!");
+    clearInterval(countDown);
+  }
+}, myInterval);
