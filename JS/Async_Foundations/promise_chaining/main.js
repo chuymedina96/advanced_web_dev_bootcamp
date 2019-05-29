7199bf7394782805a7a6317e85b102ab4c28a7f6
neo <chuymedina96@gmail.com>
2019-05-29 18:28:17 -0500

    more on promises

--- JS/Async_Foundations/promise_chaining/main.js
+++ JS/Async_Foundations/promise_chaining/main.js
@@ -12,5 +12,56 @@ var counter = 0;
 
 setTimeout(function(){
   counter++;
+  console.log("Counter:", counter);
+  setTimeout(function(){
+    counter++;
+    console.log("Counter:", counter);
+    setTimeout(function(){
+      counter++;
+      console.log("Counter:", counter);
+    }), 3000);
+  }), 2000);
+}), 1000);
 
-})
+
+var counter 0;
+
+function incCounter(){
+  counter++;
+  console.log("Counter:", counter);
+}
+
+//Code abover is hard to read expecially when the code gets longer.
+// Logic is hard to reason with.
+
+var promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    randomInt = Math.floor(Math.random()*10);
+    resolve(randomInt);
+  }, 3000);
+});
+
+promise.then(function(data){
+  console.log("Random integer passed to resolve:", data);
+  return new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve(Math.floor(Math.random()*10))
+    }, 500);
+  });
+}).then(function(data){
+  console.log("random int that was resolved is:", data);
+});
+
+//Promise chaining: returning Data.
+
+var promise = new Promise(function(resolve, reject){
+  resolve(5);
+});
+
+promise.then(function(data){
+  return data * 2;
+}).then(function(data){
+  return data + 20;
+}).then(function(data){
+  console.log(data);
+});
